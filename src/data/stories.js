cf323552710be11679aaf46c58068bec62ca91d2
Sunnnyyy16 <yjs98101@gmail.com>
2025-04-10 12:04:16 +0900

    NAVBAR 디자인수정

--- src/data/stories.js
+++ src/data/stories.js
@@ -4,7 +4,7 @@ import storyAudio from "../assets/turtle_rabbit_1.wav";
 const stories = [
     {
       id: 1,
-      title: "토끼와 거북이",
+      title: "sample1",
       pages: [
         {
           text: "옛날 옛적에 토끼와 거북이가 살았어요.",
